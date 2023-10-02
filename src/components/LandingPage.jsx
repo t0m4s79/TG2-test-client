f9be8739c6fcc685672f7c67d5d3364b1a1b730b
Tomás Marcos <50717794+t0m4s79@users.noreply.github.com>
2023-10-02 15:31:31 +0100

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const LandingPage = () => {
+  const history = useHistory();
+
   // Handle language selection and redirection
   const handleLanguageSelection = (language) => {
     // You can store the selected language in a cookie, local storage, or state
     // For simplicity, let's use local storage in this example
     localStorage.setItem('selectedLanguage', language);
+
+    // Redirect to the task list with the selected language
+    history.push(`/${language}/profile`);
   };
 
   return (
     <div>
       <h2>Select Your Language</h2>
-      <button onClick={() => handleLanguageSelection('en')}>
-        <Link to="/en/profile">English</Link>
-      </button>
-      <button onClick={() => handleLanguageSelection('pt')}>
-        <Link to="/pt/profile">Portuguese</Link>
-      </button>
+      <button onClick={() => handleLanguageSelection('en')}>English</button>
+      <button onClick={() => handleLanguageSelection('pt')}>Portuguese</button>
     </div>
   );
 };
