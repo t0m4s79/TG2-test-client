1cbc410390098e74e3fc6a23ce95199a103a6785
Tomás Marcos <50717794+t0m4s79@users.noreply.github.com>
2023-10-02 16:18:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
 
             <Routes>
-                
+                <Route path="/" element={<LandingPage />} />
                 <Route path="/:language" element={<Navbar />} />
             </Routes>
 
